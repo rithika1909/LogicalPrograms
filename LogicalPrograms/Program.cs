13a6141c3d95240ccdf931c47d93ffdb554d6b07
Rithika L <rithika.l@tvsnext.com>
2023-07-15 08:32:08 +0530

    [Rithika] Add : Implemented

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -5,15 +5,40 @@ namespace LogicalPrograms
     {
         static void Main(string[] args)
         {
-            //Fibonacci fibonacci = new Fibonacci();
-            //fibonacci.CheckNumIsFibo();
-            //PrimeNumber primeNumber = new PrimeNumber();
-            //primeNumber.CheckNumberIsPrime();
-            //PerfectNumber perfectNumber = new PerfectNumber();
-            //perfectNumber.CheckPerfectNumber();
-            ReverseNumber reverseNumber = new ReverseNumber();
-            reverseNumber.PrintReverseNumber();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter input to be executed\n 1.Fibonacci Series\n 2.Perfect Number\n" +
+                    " 3.Prime Number\n 4.Reverse Number\n 5.Stop Watch\n 6.Exit\n");
+                int input = Convert.ToInt32(Console.ReadLine());
+                switch (input)
+                {
+                    case 1:
+                        Fibonacci fibonacci = new Fibonacci();
+                        fibonacci.CheckNumIsFibo();
+                        break;
+                    case 2:
+                        PerfectNumber perfectNumber = new PerfectNumber();
+                        perfectNumber.CheckPerfectNumber();
+                        break;
+                    case 3:
+                        PrimeNumber primeNumber = new PrimeNumber();
+                        primeNumber.CheckNumberIsPrime();
+                        break;
+                    case 4:
+                        ReverseNumber reverseNumber = new ReverseNumber();
+                        reverseNumber.PrintReverseNumber();
+                        break;
+                    case 5:
+                        StopWatch stopWatch = new StopWatch();
+                        stopWatch.Run();
+                        break;
+                    case 6:
+                        flag = false;
+                        break;
 
+                }
+            }
         }
     }
 
